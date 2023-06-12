81724fe12579c0fbcd17028023307642bd488531
강경훈 <kyunghun@gang-gyeonghuns-MacBook-Pro.local>
2023-06-12 17:13:17 +0900

    ADD: 초기 셋팅

---
+++ kyunghun/app.js
@@ -0,0 +1,34 @@
+require('dotenv').config();
+
+const express = require('express');
+const logger = require('morgan');
+const cors = require('cors');
+const { DataSource } = require('typeorm');
+
+const appDataSource = new DataSource({
+  type: process.env.DB_CONNECTION,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+});
+
+appDataSource.initialize().then(() => {
+  console.log('Data Source has been initialized!');
+});
+
+const app = express();
+
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+
+app.get('/ping', function (req, res, next) {
+  res.json({ message: 'pong' });
+});
+
+const port = process.env.PORT;
+app.listen(port, function () {
+  console.log(`server listening on port ${port}`);
+});
