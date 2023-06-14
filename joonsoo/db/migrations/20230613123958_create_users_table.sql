7e24c1f5ad7a158b1e67c5abd60254a3bdad3160
joonsooha123 <135545450+joonsooha123@users.noreply.github.com>
2023-06-14 10:38:29 +0900

    MODIFY: 테이블 column 중 email에 UNIQUE옵션 추가

--- joonsoo/db/migrations/20230613123958_create_users_table.sql
+++ joonsoo/db/migrations/20230613123958_create_users_table.sql
@@ -3,7 +3,7 @@ CREATE TABLE users
 (
   id INT NOT NULL AUTO_INCREMENT,                        
   name VARCHAR(50) NOT NULL,                         
-  email VARCHAR(200) NOT NULL,
+  email VARCHAR(200) NOT NULL UNIQUE,
   profile_image VARCHAR(1000) NULL,    
   password VARCHAR(200) NOT NULL,
   created_at TIMESTAMP NOT NULL DEFAULT NOW(),           
