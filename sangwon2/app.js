5451c61ded0253c8f8769f07e5237b9da7babe0f
KimSangWon <a65854375@gmail.com>
2023-06-15 10:01:57 +0900

    MODIFY : 테이블 데이터 오류로 DB 리셋 및 유저별 게시물 전체 조회

--- sangwon2/app.js
+++ sangwon2/app.js
@@ -52,5 +52,52 @@ app.post('/users/signup', async (req, res) => {
       ) VALUES ( ?, ?, ?, ?, ?);
       `, [name, email, profileImage, password, phoneNumber]
   );
-  res.status(201).json({ message: "sign-up complete" });
+  res.status(201).json({ message: 'userCreated' });
+});
+
+app.post('/users/posts', async (req, res) => {
+  const { userId, postImage, postPG } = req.body;
+
+  await appDataSource.query(`
+    INSERT INTO posts (
+      user_id,
+      post_image,
+      post_paragraph
+    ) VALUES (?, ?, ?);
+  `, [userId, postImage, postPG]);
+
+  res.status(201).json({ message: 'postCreated' });
+});
+
+app.get('/users/posts', async (req, res) => {
+  try {
+    const posts = await appDataSource.query(`
+      SELECT *
+      FROM posts
+    `);
+
+    res.status(200).json({ data: posts });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: '서버 오류났어 ㅠㅠ' });
+  }
+});
+
+app.get('/users/posts/search', async (req, res) => {
+  try {
+    const Viewposts = await appDataSource.query(`
+      SELECT
+        users.id AS userId,
+        posts.id AS postingId,
+        posts.post_image AS postingImageUrl,
+        posts.post_paragraph AS postingContent
+      FROM users
+        INNER JOIN posts ON posts.user_id = users.id
+`);
+
+    res.status(200).json({ data: Viewposts });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: '정신차려 상원아' });
+  }
 });
