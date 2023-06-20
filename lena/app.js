2dd164f6f1220423f91247cd01ebeb7ea0d8e0b8
lchoe24 <lena.choe02@gmail.com>
2023-06-21 06:18:09 +0900

    fixed

--- lena/app.js
+++ lena/app.js
@@ -1,12 +1,16 @@
+//built-in package
 const http = require('http');
+
+// 3rd party package
+const dotenv = require('dotenv');
+dotenv.config();
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-const dotenv = require('dotenv');
-dotenv.config();
-
 const { DataSource } = require('typeorm');
-const { error } = require('console');
+
+//custom package
+const app = express();
 
 const appDataSource = new DataSource({
     type: process.env.DB_CONNECTION,
@@ -21,17 +25,15 @@ appDataSource.initialize().then(() => {
         console.log("Data Source has been initialized!")
     });
 
-const app = express();
-
 app.use(express.json());
 app.use(cors())
 app.use(morgan('dev'))
 
 app.get('/ping', function (req, res, next) {
-    res.json({message: 'pong'})
+    res.status(400).json({message: 'pong'})
   })
 
-//회원가입 request handling
+
 app.post('/signup', async (req, res) => {
     try {
         const { name, email, profileImage, password} = req.body;
@@ -50,12 +52,10 @@ app.post('/signup', async (req, res) => {
         res.status(201).json({message: 'userCreated'});
         
     } catch (error) {
-        res.json({message: 'Failed to create user'});
-        console.error("ERROR");
-    }});
+        res.status(400).json({message: 'Failed to create user'});
+}});
 
 
-//posts
 app.post('/post', async (req, res) => {
     try {
         const {title, content, userId} = req.body;
@@ -69,33 +69,29 @@ app.post('/post', async (req, res) => {
             `,
             [title, content, userId]
         );
-        res.status(301).json({message: 'postCreated'});
+        res.status(201).json({message: 'postCreated'});
     } catch (error) {
-        res.json({message: 'Failed to create post'});
-        console.error("ERROR");
+        res.status(400).json({message: 'Failed to create post'});
     }});
 
-//get all posts
-app.get('/getallposts', async (req, res) => {
-        await appDataSource.query(
-            `SELECT 
-             users.id as userId,
-             profile_image as userProfileImage,
-             posts.id as postingId,
-             posts.content as postingContent
-             FROM users
-             INNER JOIN posts on users.id = posts.user_id
-             `,   (err, rows) => {
-               res.status(200).json({"data": rows});
-       
-    });
+app.get('/posts', async (req, res) => {
+    const posts = await appDataSource.query(
+        `
+        SELECT 
+        users.id as userId,
+        profile_image as userProfileImage,
+        posts.id as postingId,
+        posts.content as postingContent
+        FROM users
+        INNER JOIN posts on users.id = posts.user_id
+        `
+        );
+    res.status(200).json({"data": posts});
 });
 
 
-//req it by doing getpostbyuser/userid/49 (say 49 is the id num); this is just the format
-app.get('/getpostbyuser/userid/:id', async (req, res) => {
-    const givenid = req.params.id
-    // var queryData = url.parse(req.url, true).query;
+app.get('/posts/user/:userId', async (req, res) => {
+    const userId = req.params.userId
      try {
         const inner = await appDataSource.query(
             
@@ -106,7 +102,7 @@ app.get('/getpostbyuser/userid/:id', async (req, res) => {
           posts.content as postingContent
           FROM users
           INNER JOIN posts on posts.user_id = users.id
-          WHERE users.id = ?`, [givenid])
+          WHERE users.id = ?`, [userId])
 
         const outer = {
             userId : inner[0].userId,
@@ -117,24 +113,23 @@ app.get('/getpostbyuser/userid/:id', async (req, res) => {
                 postingContent: p.postingContent
             }))}
 
-        res.json({"data" : outer})
+        res.status(200).json({"data" : outer})
     } catch (error) {
-        res.status(401).json({message: `Failed to retrieve posts from user ${givenid}`});
+        res.status(400).json({message: `Failed to retrieve posts from user ${givenid}`});
     }});
 
     
-//edit post
-app.post('/editpost/userid/:user_id/postid/:post_id/content/:new_content', async (req, res) => {
-    const new_content = req.params.new_content
-    const post_id = req.params.post_id
-    const user_id = req.params.user_id
+app.post('/user/:userId/post/:postId/content/:content', async (req, res) => {
+    const content = req.params.content
+    const postId = req.params.postId
+    const userId = req.params.userId
 
     try {
         await appDataSource.query(
             `UPDATE posts
             SET posts.content = ?
             WHERE posts.id = ? and posts.user_id = ?`,
-            [new_content, post_id, user_id]
+            [content, postId, userId]
         )
 
         const newPost = await appDataSource.query(
@@ -147,64 +142,47 @@ app.post('/editpost/userid/:user_id/postid/:post_id/content/:new_content', async
             FROM users
             INNER JOIN posts on posts.user_id = users.id
             WHERE users.id = ? and posts.id = ?`, 
-            [user_id, post_id]
+            [userId, postId]
         )
+        res.status(201).json({"data" : newPost})
 
-        res.json({"data" : newPost})
-    
     } catch {
-        res.json({message: "Failed to edit post"})
+        res.status(400).json({message: "Failed to edit post"})
 }});
 
 
-app.post('/deletepost/:id', async (req, res) => {
-    const post_id = req.params.id
+app.delete('/post/:postId', async (req, res) => {
+    const postId = req.params.postId
     try {
         await appDataSource.query(
             `DELETE FROM posts
             WHERE posts.id = ?`,
-            [post_id]
+            [postId]
             )
-        res.json({message: "postingDeleted"})
+        res.status(201).json({message: "postingDeleted"})
     } catch {
-        res.json("Failed to delete post")
+        res.status(400).json("Failed to delete post")
     }
 
 })
 
-app.post('/likepost/user/:user_id/post/:post_id', async (req, res) => {
-    const user_id = req.params.user_id
-    const post_id = req.params.post_id
+app.post('/user/:userId/post/:postId/likes', async (req, res) => {
+    const userId = req.params.userId
+    const postId = req.params.postId
     try {
         await appDataSource.query(
             `INSERT INTO likes (
                 user_id,
                 post_id
             ) VALUES (?, ?)`,
-            [user_id, post_id]
+            [userId, postId]
             )
-        res.json({message: "likeCreated"})
+        res.status(201).json({message: "likeCreated"})
     } catch {
-        res.json("Failed to like post")
+        res.status(400).json("Failed to like post")
     }
 }
 );
-
-
-
-// //test function just to help test
-//  app.get('/alldata', async (req, res) => {
-//     // const givenid = req.params.id
-//     // var queryData = url.parse(req.url, true).query;
-//      await appDataSource.query(
-//          `SELECT * from users
-//           inner join posts on posts.user_id = users.id`
-//           ,   (err, rows) => {
-//             res.status(200).json({"data": rows});
-    
-//  });
-//  });
-
 
 
 const server = http.createServer(app)
