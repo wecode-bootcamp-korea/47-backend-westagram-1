8fcd62c7a50a877cbecca7683a8314eed360680a
lchoe24 <lena.choe02@gmail.com>
2023-06-21 01:12:22 +0900

    1-5

--- lena/app.js
+++ lena/app.js
@@ -2,7 +2,6 @@ const http = require('http');
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
-
 const dotenv = require('dotenv');
 dotenv.config();
 
@@ -31,31 +30,131 @@ app.get('/ping', function (req, res, next) {
     res.json({message: 'pong'})
   })
 
-
-
-
+//회원가입 request handling
 app.post('/signup', async (req, res) => {
     try {
-        const { name, email, profile_image, password} = req.body;
+        const { name, email, profileImage, password} = req.body;
 
-        const result = await appDataSource.query("INSERT INTO users (name, email, profile_image, password) VALUES (?, ?, ?, ?)", 
-        [users.name, users.email, users.profile_image, users.password])
+        await appDataSource.query(
+            `INSERT INTO users (
+                name, 
+                email, 
+                profile_image, 
+                password
+            ) VALUES (?, ?, ?, ?);
+            `,
+            [name, email, profileImage, password]
+        );
 
-        res.json({message: 'userCreated'})
+        res.status(201).json({message: 'userCreated'});
         
     } catch (error) {
-        res.json({error})
-
-    }})
-
-
+        res.json({message: 'Failed to create user'});
+        console.error("ERROR");
+    }});
 
 
+//posts
+app.post('/posty', async (req, res) => {
+    try {
+        const {title, content, userId} = req.body;
 
+        await appDataSource.query(
+            `INSERT INTO posts (
+                title,
+                content,
+                user_id
+            ) VALUES (?, ?, ?);
+            `,
+            [title, content, userId]
+        );
+        res.status(301).json({message: 'postCreated'});
+    } catch (error) {
+        res.json({message: 'Failed to create post'});
+        console.error("ERROR");
+    }});
 
+//get all posts
+app.get('/allpost', async (req, res) => {
+        await appDataSource.query(
+            `SELECT 
+             users.id as userId,
+             profile_image as userProfileImage,
+             posts.id as postingId,
+             posts.content as postingContent
+             FROM users
+             JOIN posts on users.id = posts.user_id
+             `,   (err, rows) => {
+               res.status(200).json({"data": rows});
+       
+    });
+});
 
+/*
+//one user's posts
+app.get('/onepost', async (req, res) => {
+   // const givenid = req.params.id
+   // var queryData = url.parse(req.url, true).query;
+    await appDataSource.query(
+        `SELECT 
+         users.id as userId,
+         profile_image as userProfileImage,
+         posts.id as "postings.postingId",
+         posts.content as "postings.postingContent"
+         FROM users
+         JOIN posts on users.id = posts.user_id
+         WHERE users.id= 1`//, queryData['id']
+         ,   (err, rows) => {
+           res.status(200).json({"data": rows});
+   
+});
+});
+*/
 
+//req it by doing userid/49 (say 49 is the id num); this is just the format
+app.get('/userid/:id', async (req, res) => {
+    const givenid = req.params.id
+    // var queryData = url.parse(req.url, true).query;
+     try {
+        const inner = await appDataSource.query(
+            
+         `SELECT 
+          users.id as userId,
+          users.profile_image as userProfileImage,
+          posts.id as postingId,
+          posts.content as postingContent
+          FROM users
+          INNER JOIN posts on posts.user_id = users.id
+          WHERE users.id = ?`, [givenid])
 
+        const outer = {
+            userId : inner[0].userId,
+            userProfileImage : inner[0].userProfileImage,
+            postings: inner.map((p) => ({
+                postingId : p.postingId,
+                postingTitle : p.postingTitle,
+                postingContent: p.postingContent
+            }))}
+
+        res.json({"data" : outer})
+    } catch (error) {
+        res.status(401).json({message: `Failed to retrieve posts from user ${givenid}`});
+    }});
+
+    
+
+//test function just to help test
+ app.get('/alldata', async (req, res) => {
+    // const givenid = req.params.id
+    // var queryData = url.parse(req.url, true).query;
+     await appDataSource.query(
+         `SELECT * from users
+          left join posts on posts.user_id = users.id`//, queryData['id']
+          ,   (err, rows) => {
+            res.status(200).json({"data": rows});
+    
+ });
+ });
 
 
 
--- lena/db/schema.sql
+++ lena/db/schema.sql
@@ -10,6 +10,23 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `posts`
+--
+
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `posts` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL,
+  `content` varchar(3000) DEFAULT NULL,
+  `user_id` int NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `schema_migrations`
 --
 
@@ -61,5 +78,6 @@ CREATE TABLE `users` (
 
 LOCK TABLES `schema_migrations` WRITE;
 INSERT INTO `schema_migrations` (version) VALUES
-  ('20230620063649');
+  ('20230620063649'),
+  ('20230620113658');
 UNLOCK TABLES;
