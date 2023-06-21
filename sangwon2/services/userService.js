bb767ea48a12a2d4b07458a919b95013ef2208cc
KimSangWon <a65854375@gmail.com>
2023-06-21 19:33:28 +0900

    ADD : HASHED PASSWORD.

--- sangwon2/services/userService.js
+++ sangwon2/services/userService.js
@@ -1,21 +1,24 @@
 const userDao = require('../models/userDao')
+const bcrypt = require('bcrypt')
 
 const signUp = async (name, email, profileImage, passWord, phoneNumber) => {
     const pwValidation = new RegExp(
         '^(?=.*[a-z])(?=.*\\d)(?=.*[@$!%*?&])[a-z\\d@$!%*?&]{8,20}'
     );
-    
+    const saltRounds = 12;
+
     if (!pwValidation.test(passWord)) {
         const err = new Error('CHECK_PASSWORD');
         err.statusCode = 400;
         throw err;
     }
 
+    const hashedPassword = await bcrypt.hash(passWord, saltRounds);
     const createUser = await userDao.createUser(
         name,
         email,
         profileImage,
-        passWord,
+        hashedPassword,
         phoneNumber
     );
 
